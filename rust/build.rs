b6c356f1d4e2e6e0f6c5ba2a47887d2217066466
Uacias <filgum0326@gmail.com>
2025-05-13 07:59:35 +0200

    so

--- rust/build.rs
+++ rust/build.rs
@@ -1,8 +1,4 @@
 fn main() {
-    cc::Build::new()
-        .cpp(true) // enable C++ linkage
-        .include("../cpp/include") // where to find native_lib.h
-        .file("../cpp/src/native_lib.cpp") // compile this file
-        .flag_if_supported("-std=c++17")
-        .compile("native_lib"); // output libnative_lib.a
+    println!("cargo:rustc-link-search=.");
+    println!("cargo:rustc-link-lib=dylib=native_lib");
 }
